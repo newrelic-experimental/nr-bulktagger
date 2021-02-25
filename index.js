a46385ff55081a063401cf9c87820d3eb4c9fae1
npreiser <nickpreiser@gmail.com>
2021-02-24 22:42:04 -0800

    Added handler for addcsv action

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
 
-var axios = require('axios')
+var axios = require('axios');
+const csvreader = require('csv-parser');
 const fs = require('fs');
 const readline = require('readline');
 const path = require('path');
 
+const guid_list_cache = [];
 
+var headers_cache = [];
 
-if(process.argv.length < 4)
-{
-    console.log("not enough arguments, please include an action(add/delete) and  a key:value (in that format with a colon")
-    return;
-}
+//if (process.argv.length < 4) {
+//   console.log("not enough arguments, please include an action(add/delete) and  a key:value (in that format with a colon")
+//   return;
+//}
 
 const ACTION = process.argv[2].toLowerCase().trim();
 const TAG_KEY_VALUE = process.argv[3];
 var TAG_KEY = "";
 var TAG_VALUE = "";
 
-if(ACTION === 'add')
-{
+if (ACTION === 'add') {
     var parts = TAG_KEY_VALUE.split(":");
-    if(parts.length != 2)
-    {
+    if (parts.length != 2) {
         console.log("Error parsing tag key or value, please format as:    tagkey:tagvalue")
-        return; 
+        return;
     }
-    else
-    {
-        
+    else {
+
         TAG_KEY = parts[0];
         TAG_VALUE = parts[1];
         console.log("Performing Tag Add Action: " + TAG_KEY + " : " + TAG_VALUE);
-    } 
+    }
 }
-else if (ACTION === 'delete')
-{
-   // for delete we only expect a key.
-   console.log("Performing Tag Delete Action: "+ TAG_KEY); 
-   TAG_KEY = TAG_KEY_VALUE;
+else if (ACTION === 'delete') {
+    // for delete we only expect a key.
+    console.log("Performing Tag Delete Action: " + TAG_KEY);
+    TAG_KEY = TAG_KEY_VALUE;
 }
-else
-{
+else if (ACTION === 'addcsv') {
+    console.log("Performing Tag Add Tags per CSV file, looking for entity_guid_list.csv"
+    // were ok.. look for csv file (todo) 
+}
+else {
     console.log("Error, invalid action,  valid actions are :  add or delete")
     return;
 }
@@ -50,22 +51,18 @@ var API_KEY = "";
 // fetch apk key from file called api_key.txt
 
 try {
-    if(fs.existsSync('api_key.txt')) {
+    if (fs.existsSync('api_key.txt')) {
         const data = fs.readFileSync('api_key.txt', 'utf8');
-        API_KEY = data;
-        API_KEY = data.replace(/(\r\n|\n|\r)/gm, "");
-
-        console.log("APIKEY FOUND: " + API_KEY)
+        API_KEY = data.replace(/(\r\n|\n|\r)/gm, "");// remove /r/n
+        console.log("APIKEY FOUND: " + API_KEY);
     }
-    else
-    {
+    else {
         console.log("api_key.txt file is not found, please check");
         return;
     }
 
 
-    if(API_KEY.length <= 0)
-    {
+    if (API_KEY.length <= 0) {
         console.log("invalid api key, please check file");
         return;
     }
@@ -75,89 +72,173 @@ try {
 
 console.log("=======================================");
 
-const file = readline.createInterface({
-    input: fs.createReadStream('entity_guid_list.txt'),
-    output: process.stdout,
-    terminal: false
-});
+// version 2... tags are in the guid list file... 
+if (ACTION == 'addcsv') {
 
-var sendcount = 0;
+    try {
 
-console.log("Processing GUID file...");
+        const file = readline.createInterface({
+            input: fs.createReadStream('entity_guid_list.csv'),
+            output: process.stdout,
+            terminal: false
+        });
 
-file.line
+        var sendcount = 0;
+        var linecount = 0;
+        console.log("Processing GUID CSV TAGS file...");
 
-file.on('line', (line) => {
+        // file.line
 
-    sendcount++;
-    var consolelogline = sendcount;
-    var guidval = line.trim()
-    consolelogline += ":   " + guidval;
+        var headers_cache
+        file.on('line', (line) => {
 
-    var datapayload = "";
+            if (linecount <= 0) {
+                // assume header/,, store. 
+                headers_cache = line.split(",");
+                for (var i = 0; i < headers_cache.length; i++) {
+                    headers_cache[i] = headers_cache[i].trim(); //remove all spaces 
+                }
+            }
+            else {
+                sendcount++;
+                var consolelogline = sendcount;
 
-    if(ACTION === 'add')
-    {
-        datapayload = JSON.stringify({
-            query: `mutation ($guidval: EntityGuid!, $tag_key: String!, $tag_value: String!) {
+                var parts = line.split(",");
+                var values = []
+                for (var k = 0; k < parts.length; k++) {
+                    values[k] = parts[k].trim();
+                }
+                var guidval = values[0];
+                consolelogline += ":   " + guidval;
+
+                datapayload = JSON.stringify({
+                    query: `mutation ($guidval: EntityGuid!, $tag_key1: String!, $tag_value1: String!,  $tag_key2: String!, $tag_value2: String! ) {
+    taggingAddTagsToEntity(guid: $guidval,tags: [{ key: $tag_key1, values: [$tag_value1]}, { key: $tag_key2, values: [$tag_value2]}]) {
+            errors {
+                message
+            }
+        }
+     }`,
+                    variables: { "guidval": guidval, "tag_key1": headers_cache[1], "tag_value1": values[1], "tag_key2": headers_cache[2], "tag_value2": values[2] }
+                });
+
+                var config = {
+                    method: 'post',
+                    url: 'https://api.newrelic.com/graphql',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'API-Key': API_KEY
+                    },
+                    data: datapayload
+                };
+
+                axios(config)
+                    .then(function (response) {
+                        if (response.status == 200) {
+                            consolelogline += "   ===>  success"
+                        }
+                        else {
+                            consolelogline += " failed"
+                        }
+                        console.log(consolelogline);
+                        //  console.log(JSON.stringify(response.data));
+                    })
+                    .catch(function (error) {
+                        consolelogline += " exception";
+                        console.log(consolelogline);
+                        //console.log("exception: " + error);
+                    });
+            }
+            linecount++;
+
+        });
+
+    } catch (ex1) {
+        console.log("Caught Exception during csv processing/sending: " + ex1);
+    }
+
+}
+else if (ACTION == 'add' || ACTION == 'delete') {  //  Line by line,,, guid only way  v1 
+    const file = readline.createInterface({
+        input: fs.createReadStream('entity_guid_list.txt'),
+        output: process.stdout,
+        terminal: false
+    });
+
+
+    var sendcount = 0;
+
+    console.log("Processing GUID file...");
+
+    file.line
+
+    file.on('line', (line) => {
+
+        sendcount++;
+        var consolelogline = sendcount;
+        var guidval = line.trim()
+        consolelogline += ":   " + guidval;
+
+        var datapayload = "";
+
+        if (ACTION === 'add') {
+            datapayload = JSON.stringify({
+                query: `mutation ($guidval: EntityGuid!, $tag_key: String!, $tag_value: String!) {
         taggingAddTagsToEntity(guid: $guidval,tags: { key: $tag_key, values: [$tag_value]}) {
                 errors {
                     message
                 }
             }
          }`,
-            variables: {"guidval":guidval, "tag_key": TAG_KEY, "tag_value": TAG_VALUE}
-        });
-    }
-    else
-    {
-        datapayload = JSON.stringify({
-            query: `mutation ($guidval: EntityGuid!, $tag_key: String!, $tag_value: String!) {
+                variables: { "guidval": guidval, "tag_key": TAG_KEY, "tag_value": TAG_VALUE }
+            });
+        }
+        else {
+            datapayload = JSON.stringify({
+                query: `mutation ($guidval: EntityGuid!, $tag_key: String!, $tag_value: String!) {
         taggingDeleteTagsToEntity(guid: $guidval,tagKeys:  $tag_key) {
                 errors {
                     message
                 }
             }
          }`,
-            variables: {"guidval":guidval, "tag_key": TAG_KEY}
-        });
-    }
+                variables: { "guidval": guidval, "tag_key": TAG_KEY }
+            });
+        }
+
+        var config = {
+            method: 'post',
+            url: 'https://api.newrelic.com/graphql',
+            headers: {
+                'Content-Type': 'application/json',
+                'API-Key': API_KEY
+            },
+            data: datapayload
+        };
+
+        axios(config)
+            .then(function (response) {
+                if (response.status == 200) {
+                    consolelogline += "   ===>  success"
+                }
+                else {
+                    consolelogline += " failed"
+                }
+                console.log(consolelogline);
+                //  console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                consolelogline += " exception";
+                console.log(consolelogline);
+                //console.log("exception: " + error);
+            });
+    });
 
 
-  
+    file.on('close', () => {
 
-    var config = {
-        method: 'post',
-        url: 'https://api.newrelic.com/graphql',
-        headers: {
-            'Content-Type': 'application/json',
-            'API-Key': API_KEY
-        },
-        data : datapayload
-    };
-
-    axios(config)
-        .then(function (response) {
-            if(response.status == 200) {
-                consolelogline += "   ===>  success"
-            }
-            else {
-                consolelogline += " failed"
-            }
-            console.log(consolelogline);
-          //  console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
-            consolelogline += " exception";
-            console.log(consolelogline);
-            //console.log("exception: " + error);
-        });
-});
+        // console.log("Bulk tag processing is complete." + sendcount + " items");
+    });
 
 
-
-file.on('close', () => {
-
-   // console.log("Bulk tag processing is complete." + sendcount + " items");
-});
-
+}
